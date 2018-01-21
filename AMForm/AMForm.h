f9f4a9abef09f41225a63a74e7f607ac65b9ae0d
Anthony Merle <merle.anthony0@gmail.com>
2018-01-21 19:14:01 +0100

    Initial commit

---
+++ AMForm/AMForm.h
@@ -0,0 +1,19 @@
+//
+//  AMForm.h
+//  AMForm
+//
+//  Created by Anthony Merle on 21/01/2018.
+//  Copyright © 2018 Anthony Merle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMForm.
+FOUNDATION_EXPORT double AMFormVersionNumber;
+
+//! Project version string for AMForm.
+FOUNDATION_EXPORT const unsigned char AMFormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMForm/PublicHeader.h>
+
+
